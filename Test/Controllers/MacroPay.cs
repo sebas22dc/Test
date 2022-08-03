dad89db85bf6001baa60e5ea9c58c7eb7b5a74c6
Sebastian dzul <sebassdc@hotmail.com>
2022-08-03 12:45:35 -0500

    Add project files.

---
+++ Test/Controllers/MacroPay.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
+using Test.Models;
+using Test.Utilities;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Route("Api/v1")]
+    public class MacroPay : ControllerBase
+    {
+        List<AdressBook> Db = new ReadDb().ReadFakeDb();
+
+
+        [HttpGet]
+        [Route("contacts")]
+        public IActionResult contacts()
+        {
+            var QueryParams = ControllerContext.HttpContext.Request.Query;
+            if (QueryParams.TryGetValue("phrase", out StringValues value))
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return BadRequest("Error en la solicitud");
+                }
+            }
+            string phrase = value.ToString();
+            List<AdressBook> items = Db;
+            if (phrase != null && phrase != "")
+            {
+                items = Db.FindAll(I => I.name.ToLower().Contains(phrase.ToLower()));
+            }
+            return Ok(items);
+        }
+
+
+        [HttpGet]
+        [Route("contacts/{ID?}")]
+        public IActionResult ContactId(string? ID = "")
+        {
+            AdressBook item = Db.Find(I => I.id == ID);
+            if (item == null)
+            {
+                return new NotFoundObjectResult("Elemento no encontrado");
+            }
+            return new OkObjectResult(item);
+        }
+
+        [HttpDelete]
+        [Route("contacts/{ID?}")]
+        public IActionResult DeleteContact(string? ID = "")
+        {
+            AdressBook item = Db.Find(I => I.id == ID);
+            if (item == null)
+            {
+                return new NotFoundObjectResult("Elemento no encontrado"); ;
+            }
+            new ReadDb().UpdateDeleteDb(Db.FindAll(I => I.id != ID));
+            return new NoContentResult();
+        }
+    }
+}
---
+++ Test/Models/AdressBook.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Test.Models
+{
+    public class AdressBook
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public List<string> addressLines { get; set; }
+    }
+}
